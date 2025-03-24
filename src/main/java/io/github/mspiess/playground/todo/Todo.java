51d61c7f960d18b5d13b088ad804bdc14c842c65
mspiess <marco.spiess@hotmail.de>
2025-03-24 16:15:50 +0100

    fix: fix ID generation

--- src/main/java/io/github/mspiess/playground/todo/Todo.java
+++ src/main/java/io/github/mspiess/playground/todo/Todo.java
@@ -1,6 +1,13 @@
 package io.github.mspiess.playground.todo;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import java.util.UUID;
 
-public record Todo(UUID id, String title) {
+public record Todo(
+        @JsonIgnore
+        Long id,
+        UUID publicId,
+        String title
+) {
 }
--- src/test/java/io/github/mspiess/playground/todo/TodoTest.java
+++ src/test/java/io/github/mspiess/playground/todo/TodoTest.java
@@ -2,18 +2,24 @@ package io.github.mspiess.playground.todo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.graphql.tester.AutoConfigureGraphQlTester;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.graphql.test.tester.GraphQlTester;
+import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
+@AutoConfigureGraphQlTester
 @Transactional
 public class TodoTest {
 
     @Autowired
     GraphQlTester graphQlTester;
+
+    @Autowired
+    JdbcClient jdbcClient;
 
     @Test
     void queryTodosReturnsEmptyList() {
@@ -32,6 +38,34 @@ public class TodoTest {
                 .execute()
                 .errors()
                 .verify()
-                .path("getTodos.edges", path -> path.entityList(Object.class).satisfies(list -> assertThat(list).isEmpty()));
+                .path("getTodos.edges")
+                .entityList(Object.class)
+                .hasSize(0);
+    }
+
+    @Test
+    void queryTodosReturnsTodos() {
+        int affectedCount = jdbcClient.sql("INSERT INTO todo (title) VALUES (?)").param("Some Title").update();
+        assertThat(affectedCount).isEqualTo(1);
+
+        graphQlTester.document(
+                        """
+                                {
+                                    getTodos {
+                                        edges {
+                                            node {
+                                                title
+                                            }
+                                        }
+                                    }
+                                }
+                                """)
+                .execute()
+                .errors()
+                .verify()
+                .path("getTodos.edges[*].node")
+                .entityList(Todo.class)
+                .hasSize(1)
+                .satisfies(list -> assertThat(list.getFirst().title()).isEqualTo("Some Title"));
     }
 }
